f4da4c69e1beb668301cf881ae2d0174e318deb6
akash-soni <akash.200287@gmail.com>
2023-04-21 08:21:42 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+## edit below variables as per your requirements -
+REPO_NAME = "ML-project-template"
+AUTHOR_USER_NAME = "akash-soni"
+
+
+setup(
+    name=REPO_NAME,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for MLflow app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="aakashsoni.official@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    license="MIT",
+)
