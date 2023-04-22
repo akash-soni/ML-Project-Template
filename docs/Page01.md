9ba488dff0d24d6c61f78c8a3c28bb59fc774f91
akash-soni <akash.200287@gmail.com>
2023-04-23 00:41:52 +0530

    corrected

---
+++ docs/Page01.md
@@ -0,0 +1,7 @@
+# This is page 1
+
+# Data Preperation Steps
+
+In this step we will convert strings to numerical tokens and we will also apply necessary preprocessing on data tags
+
+## Tag Objects
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+from src.logger import logging
+from src.utils.common import read_yaml, create_directories
+import random
+
+
+STAGE = "STAGE_NAME"  ## <<< change stage name
+
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    pass
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
